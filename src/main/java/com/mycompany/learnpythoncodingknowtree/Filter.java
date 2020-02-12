309a0849d4e10e24827bc02921c1200895e3f442
zoe <test@com.tw>
2020-02-12 11:04:31 +0800

    Merge origin/master
    Conflicts:
    	pom.xml
    	src/main/java/imsofa/ilab/wesop/rollerplugin/editor/SessionConfigEditEditor.java
    	src/main/java/imsofa/ilab/wesop/rollerplugin/model/SessionDetailTableModel.java

--- src/main/java/com/mycompany/learnpythoncodingknowtree/Filter.java
+++ src/main/java/com/mycompany/learnpythoncodingknowtree/Filter.java
@@ -7,6 +7,8 @@ package com.mycompany.learnpythoncodingknowtree;
 
 import com.google.gson.Gson;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
 import java.util.List;
 import java.util.Map;
 import org.apache.commons.io.FileUtils;
@@ -20,15 +22,27 @@ public class Filter {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         // TODO code application logic here
-        String str=FileUtils.readFileToString(new File("dlist_json.txt"), "big5");
+        String str = FileUtils.readFileToString(new File("dlist_json.txt"), "big5");
+        File dlist_csv = new File("dlist.csv");
+        OutputStreamWriter ow = new OutputStreamWriter(new FileOutputStream(dlist_csv), "utf-8");
         System.out.println(str);
-        Gson gson=new Gson();
-        List<Map> list=gson.fromJson(str, List.class);
-        for(Map data : list){
-            System.out.println(data.get("answer_count"));
+        Gson gson = new Gson();
+        ow.write("question_id,view_count,answer_count,score\r\n");
+        List<Map> list = gson.fromJson(str, List.class);
+        for (Map data : list) {
+            ow.write(data.get("question_id").toString());
+            ow.write(",");
+            ow.write(data.get("view_count").toString());
+            ow.write(",");
+            ow.write(data.get("answer_count").toString());
+            ow.write(",");
+            ow.write(data.get("score").toString());
+            ow.write("\r\n");
+            System.out.println(data.get("question_id").toString());
         }
+        ow.close();
     }
-    
+
 }
